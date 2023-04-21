44eeae6e39d539ead997ac960ea204392d2b3efc
Starglancer <starglancer.x@pm.me>
2023-04-21 16:19:13 +0100

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports Nimiq
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Cfg As Config = New Config()
+
+        Cfg.Scheme = "http"
+        Cfg.Host = "localhost"
+        Cfg.Port = 8648
+
+        Dim Client As NimiqClient = New NimiqClient(Cfg)
+
+        Dim blocknumber As Integer = Client.BlockNumber
+
+        Label1.Text = blocknumber
+
+    End Sub
+
+End Class
